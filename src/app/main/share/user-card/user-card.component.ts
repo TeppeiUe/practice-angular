f334a6e6d002c6f585ae791b4d4712e5ef0d9c51
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2022-12-30 08:35:56 +0900

    [ADD] status-based following button view template

--- src/app/main/share/user-card/user-card.component.ts
+++ src/app/main/share/user-card/user-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user-params';
+import { AuthService } from 'src/app/services/auth.service';
+import { FollowService } from 'src/app/services/follow.service';
 
 @Component({
   selector: 'app-user-card',
@@ -8,10 +10,33 @@ import { User } from 'src/app/models/user-params';
 })
 export class UserCardComponent implements OnInit {
   @Input() userList: User[]|[] = [];
+  private followingList: number[] = [];
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private follow: FollowService,
+  ) { }
 
   ngOnInit(): void {
+    this.auth.user$.subscribe(user => {
+      if (user) {
+        this.follow.getUserFollowingList(user.id).subscribe(followings => {
+          this.followingList = followings.map(f => f.id);
+        });
+      }
+    })
+  }
+
+  public setFollowing(user_id: number):boolean {
+    return this.followingList.includes(user_id)
+  }
+
+  public deleteFollowing(ind: number) {
+    this.followingList = this.followingList.filter(f => f !== ind);
+  }
+
+  public addFollowing(ind: number) {
+    this.followingList.push(ind);
   }
 
 }

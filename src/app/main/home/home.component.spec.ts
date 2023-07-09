cc4f01ac6fbbe8b154cd92f76dbafca8e516a10a
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2023-07-09 23:09:02 +0900

    [ADD] home.component unit test

--- src/app/main/home/home.component.spec.ts
+++ src/app/main/home/home.component.spec.ts
@@ -1,14 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { AuthService } from 'src/app/services/auth.service';
+import { TweetService } from 'src/app/services/tweet.service';
+import { of } from 'rxjs';
+import { User } from 'src/app/models/user-params';
+import { Tweet } from 'src/app/models/tweet-params';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let authService: jasmine.SpyObj<AuthService>;
+  let tweetService: jasmine.SpyObj<TweetService>;
+
+  const user: User = {
+    id: 1,
+    user_name: '',
+    profile: '',
+    image: '',
+  };
+
+  const tweet: Tweet = {
+    id: 1,
+    message: '',
+    user_id: 1,
+    created_at: new Date(),
+  }
 
   beforeEach(async () => {
+    authService = jasmine.createSpyObj('AuthService', [], {
+      user$: of(user)
+    });
+    tweetService = jasmine.createSpyObj('TweetService', [], {
+      tweet$: of(tweet)
+    });
+
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      providers: [
+        { provide: AuthService, useValue: authService },
+        { provide: TweetService, useValue: tweetService },
+      ]
     })
     .compileComponents();
 
